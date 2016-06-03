3e66a717f64f0134511c6f13218ccf8d99013451
TyIsI <tycho.eggen@gmail.com>
2016-06-03 13:29:31 -0700

    Optimization

--- colore.js
+++ colore.js
@@ -7,10 +7,25 @@ function ColoreStateMachine() {
 	this.init = function() {
 		$(document).ready(function() {
 			$(window).on('hashchange', function() {
-				var hash = location.hash.substring(1);
-				self.loadContext( hash );
+				self.update();
 			});
+			self.update();
 		});
+	}
+	
+	this.getHash = function() {
+		var hash = location.hash;
+		
+		if( hash == '' )
+			return false;
+		
+		if( hash.substring(0,1) == '\?' )
+			hash = hash.substring(1);
+		
+		if( hash.substring(0,1) == '#' )
+			hash = hash.substring(1);
+		
+		return hash;
 	}
 
 	this.addContext = function( context, handler ) {
@@ -19,22 +34,34 @@ function ColoreStateMachine() {
 
 	this.loadContext = function( context ) {
 		if( this.contexts[context] !== undefined ) {
+			console.log( "ColoreStateMachine.loadContext: Loading '" + context + "'" );
 			this.context = context;
 			this.contexts[context]();
 		} else {
-			console.log( "Colore: Missing context" );
+			console.log( "ColoreStateMachine.loadContext: Missing context '" + context + "'" );
 		}
 	}
 	
 	this.setDefaultContext = function( context ) {
+		console.log( "ColoreStateMachine.setDefaultContext: " + context );
 		this.settings['default'] = context;
-		if( this.context == "" ) {
-			this.go( context );
-		}
 	}
 	
 	this.go = function( context ) {
 		location.hash = context;
+	}
+	
+	this.update = function() {
+		var hash = this.getHash();
+		
+		console.log( "ColoreStateMachine.update: Detecting context: [" + hash + "]" );
+		
+		if( hash != false ) {
+			this.loadContext( hash );
+		} else if( this.context == "" ) {
+			this.go( this.settings['default'] );
+		}
+
 	}
 	
 	this.init();
